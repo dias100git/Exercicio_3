cad3b9d10c6b4df507e27b6deb69a0c1c30b6bf1
dias100git <lucio100dias@gmail.com>
2023-05-07 01:54:43 +0100

    First commit

---
+++ Exercicio_3/Estudante.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_3
+{
+    internal class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ Exercicio_3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Exercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.WriteLine("Indique o número de quartos a alugar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("ID do Aluguer: " + i);
+                Console.Write("Nome do aluno: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email do aluno: ");
+                string email = Console.ReadLine();
+                Console.Write("Número do Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+
+
+                }
+
+            }
+        }
+    }
+}
